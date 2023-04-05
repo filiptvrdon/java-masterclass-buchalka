3bb3f350ba41738a643d0abcbf7eb442df27e7ea
Filip Tvrdoň <tvrdon.filip@gmail.com>
2023-04-05 07:00:16 +0200

    lesson 304 finished

--- InputOutputNIO/src/com/timbuchalka/Locations.java
+++ InputOutputNIO/src/com/timbuchalka/Locations.java
@@ -14,54 +14,37 @@ public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new LinkedHashMap<Integer, Location>();
 
     public static void main(String[] args) throws IOException {
-       Path locPath = FileSystems.getDefault().getPath("locations_big.txt");
-       Path dirPath = FileSystems.getDefault().getPath("directions_big.txt");
+       Path locPath = FileSystems.getDefault().getPath("locations.dat");
 
-       try (BufferedWriter locFile = Files.newBufferedWriter(locPath);
-            BufferedWriter dirFile = Files.newBufferedWriter(dirPath)) {
-           for (Location location : locations.values()) {
-               locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
-               for (String direction : location.getExits().keySet()) {
-                   if (!direction.equalsIgnoreCase("Q")) {
-                       dirFile.write(location.getLocationID() + "," + direction + "," + location.getExits().get(direction) + "\n");
-                   }
-               }
+       try (ObjectOutputStream locFile = new ObjectOutputStream(new BufferedOutputStream(Files.newOutputStream(locPath)))){
+           for (Location location : locations.values()){
+               locFile.writeObject(location);
            }
-       } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
        }
+
     }
 
     static {
-        Path locPath = FileSystems.getDefault().getPath("locations_big.txt");
-        Path dirPath = FileSystems.getDefault().getPath("directions_big.txt");
-
-        try (Scanner scanner = new Scanner(Files.newBufferedReader(locPath))){
-            scanner.useDelimiter(",");
-            while (scanner.hasNextLine()){
-                int loc = scanner.nextInt();
-                scanner.skip(scanner.delimiter());
-                String description = scanner.nextLine();
-                System.out.println("Imported loc: " + loc + ": " + description);
-                locations.put(loc, new Location(loc, description, null));
+        Path locPath = FileSystems.getDefault().getPath("locations.dat");
+//        Path dirPath = FileSystems.getDefault().getPath("directions.dat");
+        try (ObjectInputStream locFile = new ObjectInputStream(new BufferedInputStream(Files.newInputStream(locPath)))){
+            boolean eof = false;
+            while (!eof){
+                try {
+                    Location location = (Location) locFile.readObject();
+                    System.out.println("Read location " + location.getLocationID() + ": " + location.getDescription());
+                    System.out.println("Found " + location.getExits().size() + " exits");
+                    locations.put(location.getLocationID(), location);
+                } catch (EOFException e){
+                    eof = true;
+                }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try (BufferedReader dirFile = Files.newBufferedReader(dirPath)){
-            String input;
-            while ((input = dirFile.readLine()) != null){
-                String[] data = input.split(",");
-                int loc = Integer.parseInt(data[0]);
-                String direction = data[1];
-                int destination = Integer.parseInt(data[2]);
-                System.out.println(loc + ": " + direction + ": " + destination);
-                Location location = locations.get(loc);
-                location.addExit(direction, destination);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (InvalidClassException e){
+            System.out.println("InvalidClassException: " + e.getMessage());
+        } catch (IOException io){
+            System.out.println("IO Exception");
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException: " + e.getMessage());
         }
     }
 

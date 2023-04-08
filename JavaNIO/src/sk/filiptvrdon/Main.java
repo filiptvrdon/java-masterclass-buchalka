d0d899437baff0e50b1978f5a3338a72d70797ce
Filip Tvrdoň <tvrdon.filip@gmail.com>
2023-04-08 06:06:24 +0200

    lesson 311 before threads

--- JavaNIO/src/sk/filiptvrdon/Main.java
+++ JavaNIO/src/sk/filiptvrdon/Main.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
@@ -54,50 +53,17 @@ public class Main {
             readBuffer.flip();
             System.out.println("int1 = " + readBuffer.getInt());
 
-            // write the integers into the file in the same positions as before using random access
-//            ByteBuffer writeBuffer = ByteBuffer.allocate(Integer.BYTES);
-//            writeBuffer.putInt(245);
-//            writeBuffer.flip();
-//            channel.write(writeBuffer, int1Pos);
-//            writeBuffer.flip();
-//            writeBuffer.putInt(-98765);
-//            writeBuffer.flip();
-//            channel.write(writeBuffer, int2Pos);
-//            writeBuffer.flip();
-//            writeBuffer.putInt(1000);
-//            writeBuffer.flip();
-//            channel.write(writeBuffer, int3Pos);
 
-            byte[] outputString = "Hello, World!".getBytes();
-            long str1Pos = 0;
-            long newInt1Pos = outputString.length;
-            long newInt2Pos = newInt1Pos + Integer.BYTES;
-            byte[] outputString2 = "Nice to meet you".getBytes();
-            long str2Pos = newInt2Pos + Integer.BYTES;
-            long newInt3Pos = str2Pos + outputString2.length;
+            channel.position(0); // set the position to the beginning of the file
+            RandomAccessFile copyFile = new RandomAccessFile("datacopy.dat", "rw");
+            FileChannel copyChannel = copyFile.getChannel();
+            // long numTransferred = copyChannel.transferFrom(channel, 0, channel.size());
+            long numTransferred = channel.transferTo(0, channel.size(), copyChannel);
+            System.out.println("Num transferred = " + numTransferred);
+            channel.close();
+            ra.close();
+            copyChannel.close();
 
-            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
-            intBuffer.putInt(245);
-            intBuffer.flip();
-            binChannel.position(newInt1Pos);
-            binChannel.write(intBuffer);
-            intBuffer.flip();
-
-            intBuffer.putInt(-98765);
-            intBuffer.flip();
-            binChannel.position(newInt2Pos);
-            binChannel.write(intBuffer);
-            intBuffer.flip();
-
-            intBuffer.putInt(1000);
-            intBuffer.flip();
-            binChannel.position(newInt3Pos);
-            binChannel.write(intBuffer);
-
-            binChannel.position(str1Pos);
-            binChannel.write(ByteBuffer.wrap(outputString));
-            binChannel.position(str2Pos);
-            binChannel.write(ByteBuffer.wrap(outputString2));
 
         } catch (IOException e) {
             e.printStackTrace();

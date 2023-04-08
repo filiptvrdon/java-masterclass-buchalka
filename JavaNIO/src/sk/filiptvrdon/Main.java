dfd3aa761ab70dda139648ada0d5ca567410c645
Filip Tvrdoň <tvrdon.filip@gmail.com>
2023-04-08 05:53:44 +0200

    lesson 310 finished

--- JavaNIO/src/sk/filiptvrdon/Main.java
+++ JavaNIO/src/sk/filiptvrdon/Main.java
@@ -15,17 +15,6 @@ public class Main {
              FileChannel binChannel = binFile.getChannel()) {
 
             ByteBuffer buffer = ByteBuffer.allocate(100);
-            //byte[] outputBytes = "Hello World!".getBytes();
-            // byte[] outputBytes2 = "Nice to meet you".getBytes();
-
-            // put method returns the buffer so we can chain the methods
-//            buffer.put(outputBytes)
-//                  .putInt(245)
-//                  .putInt(-98765)
-//                  .put(outputBytes2)
-//                  .putInt(1000);
-//            buffer.flip(); // flip method sets the position to 0 so that when buffer is written to the file, it starts from the beginning
-
 
             byte[] outputBytes = "Hello World!".getBytes();
             buffer.put(outputBytes);
@@ -45,32 +34,70 @@ public class Main {
             buffer.flip(); // sets the position to 0 so that when buffer is written to the file, it starts from the beginning
             binChannel.write(buffer);
 
-            // print out the int positions in the file
-            System.out.println("int1Pos = " + int1Pos);
-            System.out.println("int2Pos = " + int2Pos);
-            System.out.println("int3Pos = " + int3Pos);
 
-
+            // read the integers from the file in reverse order
             RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
             FileChannel channel = ra.getChannel();
-            ByteBuffer readBuffer = ByteBuffer.allocate(100);
+            ByteBuffer readBuffer = ByteBuffer.allocate(Integer.BYTES);
+            channel.position(int3Pos);
             channel.read(readBuffer);
             readBuffer.flip();
-            byte[] inputString = new byte[outputBytes.length];
-            readBuffer.get(inputString);
-            System.out.println("inputString = " + new String(inputString));
-            System.out.println("int1 = " + readBuffer.getInt());
-            System.out.println("int2 = " + readBuffer.getInt());
-            byte[] inputString2 = new byte[outputBytes2.length];
-            readBuffer.get(inputString2);
-            System.out.println("inputString2 = " + new String(inputString2));
             System.out.println("int3 = " + readBuffer.getInt());
+            readBuffer.flip();
+            channel.position(int2Pos);
+            channel.read(readBuffer);
+            readBuffer.flip();
+            System.out.println("int2 = " + readBuffer.getInt());
+            readBuffer.flip();
+            channel.position(int1Pos);
+            channel.read(readBuffer);
+            readBuffer.flip();
+            System.out.println("int1 = " + readBuffer.getInt());
 
+            // write the integers into the file in the same positions as before using random access
+//            ByteBuffer writeBuffer = ByteBuffer.allocate(Integer.BYTES);
+//            writeBuffer.putInt(245);
+//            writeBuffer.flip();
+//            channel.write(writeBuffer, int1Pos);
+//            writeBuffer.flip();
+//            writeBuffer.putInt(-98765);
+//            writeBuffer.flip();
+//            channel.write(writeBuffer, int2Pos);
+//            writeBuffer.flip();
+//            writeBuffer.putInt(1000);
+//            writeBuffer.flip();
+//            channel.write(writeBuffer, int3Pos);
 
+            byte[] outputString = "Hello, World!".getBytes();
+            long str1Pos = 0;
+            long newInt1Pos = outputString.length;
+            long newInt2Pos = newInt1Pos + Integer.BYTES;
+            byte[] outputString2 = "Nice to meet you".getBytes();
+            long str2Pos = newInt2Pos + Integer.BYTES;
+            long newInt3Pos = str2Pos + outputString2.length;
 
+            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+            intBuffer.putInt(245);
+            intBuffer.flip();
+            binChannel.position(newInt1Pos);
+            binChannel.write(intBuffer);
+            intBuffer.flip();
 
+            intBuffer.putInt(-98765);
+            intBuffer.flip();
+            binChannel.position(newInt2Pos);
+            binChannel.write(intBuffer);
+            intBuffer.flip();
 
+            intBuffer.putInt(1000);
+            intBuffer.flip();
+            binChannel.position(newInt3Pos);
+            binChannel.write(intBuffer);
 
+            binChannel.position(str1Pos);
+            binChannel.write(ByteBuffer.wrap(outputString));
+            binChannel.position(str2Pos);
+            binChannel.write(ByteBuffer.wrap(outputString2));
 
         } catch (IOException e) {
             e.printStackTrace();

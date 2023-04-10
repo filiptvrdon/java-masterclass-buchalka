7f159216def461240012a9195f9a8ad9a57b891c
Filip Tvrdoň <tvrdon.filip@gmail.com>
2023-04-10 05:57:54 +0200

    leesson 316 finished

--- Lesson_312_Filesystem/src/sk/filiptvrdon/Main.java
+++ Lesson_312_Filesystem/src/sk/filiptvrdon/Main.java
@@ -7,52 +7,60 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
 
-       try{
+        try {
+//            Path fileToCreate = FileSystems.getDefault().getPath("Examples", "file2.txt");
+//            Files.createFile(fileToCreate);
 
-//          Path fileToMove = FileSystems.getDefault().getPath("Examples", "file1.txt");
-//          Path destination = FileSystems.getDefault().getPath("Examples", "file1.txt");
-//          Files.move(fileToMove, destination);
+//            Path dirToCreate = FileSystems.getDefault().getPath("Examples", "Dir4");
+//            Files.createDirectory(dirToCreate);
 
-//            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
-//            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
-//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+//            Path dirToCreate = FileSystems.getDefault().getPath("Examples", "Dir2\\Dir3\\Dir4\\Dir5\\Dir6");
+//            Files.createDirectories(dirToCreate);
 
-//            sourceFile = FileSystems.getDefault().getPath("Examples", "Dir1");
-//            copyFile = FileSystems.getDefault().getPath("Examples", "Dir4");
-//            Files.copy(sourceFile, copyFile);
+//            Path filePath = FileSystems.getDefault().getPath("Examples", "Dir2\\Dir3\\Dir4\\Dir5\\Dir6\\Dir7");
+//            Files.createDirectories(filePath);
+
+            Path filePath = FileSystems.getDefault().getPath("Examples", "Dir1\\file1.txt");
+            System.out.println("Size = " + Files.size(filePath));
+            System.out.println("Last modified = " + Files.getLastModifiedTime(filePath));
+            System.out.println("Is writable = " + Files.isWritable(filePath));
+            System.out.println("Is readable = " + Files.isReadable(filePath));
+            System.out.println("Is executable = " + Files.isExecutable(filePath));
+
+            BasicFileAttributes attributes = Files.readAttributes(filePath, BasicFileAttributes.class);
+            System.out.println("Size = " + attributes.size());
+            System.out.println("Last modified = " + attributes.lastModifiedTime());
+            System.out.println("Created = " + attributes.creationTime());
+            System.out.println("Is directory = " + attributes.isDirectory());
+            System.out.println("Is regular file = " + attributes.isRegularFile());
 
 
-           Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
-           Files.deleteIfExists(fileToDelete);
+//           Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
+//           Files.deleteIfExists(fileToDelete);
 
 
-
-
-        } catch(IOException e){
+        } catch (IOException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
 
 
-
-
-
-
-
     }
 
-    private static void printFile(Path path){
-        try(BufferedReader br = Files.newBufferedReader(path)){
+    private static void printFile(Path path) {
+        try (BufferedReader br = Files.newBufferedReader(path)) {
             String line;
-            while((line = br.readLine()) != null){
+            while ((line = br.readLine()) != null) {
                 System.out.println(line);
                 System.out.println("*****************");
             }
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
             e.printStackTrace();
         }

ea3d167e636fe7a78ed527a1feb0dad2c958262f
Filip Tvrdoň <tvrdon.filip@gmail.com>
2023-04-14 06:14:00 +0200

    Lesson 325 finished

--- Threads/src/sk/filiptvrdon/Main.java
+++ Threads/src/sk/filiptvrdon/Main.java
@@ -20,8 +20,17 @@ public class Main {
             @Override
             public void run() {
                 System.out.println(ANSI_RED + "Hello from the anonymous class's implementation of run()");
+                try {
+                    anotherThread.join(2000); // wait for anotherThread to finish or 2 seconds
+                    System.out.println(ANSI_RED + "AnotherThread terminated, or timed out, so I'm running again");
+                } catch (InterruptedException e) {
+                    System.out.println(ANSI_RED + "I couldn't wait after all. I was interrupted");
+                }
             }
         });
+        myRunnableThread.start();
+
+//        anotherThread.interrupt();
 
 
         System.out.println(ANSI_PURPLE + "Hello again from the main thread!");

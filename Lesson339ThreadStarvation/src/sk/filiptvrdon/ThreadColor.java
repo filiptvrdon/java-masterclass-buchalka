10fcff3f239e3ec21dcd8cabcbd04bb4ccd077ec
Filip Tvrdoň <tvrdon.filip@gmail.com>
2023-04-22 13:41:45 +0200

    lesson 339 finished

---
+++ Lesson339ThreadStarvation/src/sk/filiptvrdon/ThreadColor.java
@@ -0,0 +1,11 @@
+package sk.filiptvrdon;
+
+public class ThreadColor {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m"; // 30m is the code for black
+    public static final String ANSI_RED = "\u001B[31m"; // 31m is the code for red
+    public static final String ANSI_GREEN = "\u001B[32m"; // 32m is the code for green
+    public static final String ANSI_BLUE = "\u001B[34m"; // 34m is the code for blue
+    public static final String ANSI_PURPLE = "\u001B[35m"; // 35m is the code for purple
+    public static final String ANSI_CYAN = "\u001B[36m"; // 36m is the code for cyan
+}

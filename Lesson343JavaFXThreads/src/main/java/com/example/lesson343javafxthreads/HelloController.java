4f65b10084508bacd191fa3e0a414ee285b48a84
Filip Tvrdoň <tvrdon.filip@gmail.com>
2023-04-23 13:44:47 +0200

    lesson 345 finished

--- Lesson343JavaFXThreads/src/main/java/com/example/lesson343javafxthreads/HelloController.java
+++ Lesson343JavaFXThreads/src/main/java/com/example/lesson343javafxthreads/HelloController.java
@@ -2,7 +2,10 @@ package com.example.lesson343javafxthreads;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Service;
 import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -11,7 +14,6 @@ import javafx.scene.control.ProgressBar;
 public class HelloController {
     @FXML
     private Label progressLabel;
-    private Task<ObservableList<String>> task;
 
     @FXML
     private ListView listView;
@@ -19,42 +21,45 @@ public class HelloController {
     @FXML
     private ProgressBar progressBar;
 
+    private Service<ObservableList<String>> service;
+
     @FXML
     protected void onButtonClick() {
-        // progressLabel.setText("Getting Employees...");
-        new Thread(task).start();
+        if (service.getState() == Service.State.SUCCEEDED) {
+            service.reset();
+            service.start();
+        } else if (service.getState() == Service.State.READY) {
+            service.start();
+        }
     }
 
     public void initialize() {
-        progressBar.setVisible(true);
-        task = new Task<ObservableList<String>>() {
-            @Override
-            protected ObservableList<String> call() throws Exception {
-                String[] names = {
-                        "Tim Buchalka",
-                        "Bill Rogers",
-                        "Jack Jill",
-                        "Joan Andrews",
-                        "Mary Johnson",
-                        "Bob McDonald"
-                };
+        service = new EmployeeService();
 
-                ObservableList<String> employees = FXCollections.observableArrayList();
+//        service.setOnRunning(new EventHandler<WorkerStateEvent>() {
+//            @Override
+//            public void handle(WorkerStateEvent workerStateEvent) {
+//                progressBar.setVisible(true);
+//                progressLabel.setVisible(true);
+//            }
+//        });
+//
+//        service.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+//            @Override
+//            public void handle(WorkerStateEvent workerStateEvent) {
+//                progressBar.setVisible(false);
+//                progressLabel.setVisible(false);
+//            }
+//        });
 
-                for (int i = 0; i < 6; i++) {
-                    employees.add(names[i]);
-                    updateMessage("Added " + names[i] + " to the list");
-                    updateMessage("Added " + names[i] + " to the list");
-                    updateProgress(i + 1, 6);
-                    Thread.sleep(500);
-                }
+//        progressBar.setVisible(false);
+//        progressLabel.setVisible(false);
 
-                return employees;
-            }
-        };
-        progressBar.progressProperty().bind(task.progressProperty());
-        progressLabel.textProperty().bind(task.messageProperty());
-        listView.itemsProperty().bind(task.valueProperty());
+        progressBar.visibleProperty().bind(service.runningProperty());
+        progressLabel.visibleProperty().bind(service.runningProperty());
+        progressBar.progressProperty().bind(service.progressProperty());
+        progressLabel.textProperty().bind(service.messageProperty());
+        listView.itemsProperty().bind(service.valueProperty());
 
 
     }

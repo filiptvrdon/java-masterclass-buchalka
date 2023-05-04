3221a2ae358dae63435c7652471fd52162a1b4d7
Filip Tvrdoň <tvrdon.filip@gmail.com>
2023-05-04 06:20:48 +0200

    lesson 413 finished

--- Lesson412HighLevelApis/src/sk/filiptvrdon/Main.java
+++ Lesson412HighLevelApis/src/sk/filiptvrdon/Main.java
@@ -1,43 +1,35 @@
 package sk.filiptvrdon;
 
-import javax.print.attribute.URISyntax;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
 
 public class Main {
     public static void main(String[] args) {
 
         try {
-//            URI uri = new URI("db://username:password@myserver.com:5000/catalogue/phones?os=android#samsung");
-//            System.out.println("Scheme: " + uri.getScheme());
-//            System.out.println("Scheme-specific part: " + uri.getSchemeSpecificPart());
-//            System.out.println("Authority: " + uri.getAuthority());
-//            System.out.println("User info: " + uri.getUserInfo());
-//            System.out.println("Host: " + uri.getHost());
-//            System.out.println("Port: " + uri.getPort());
-//            System.out.println("Path: " + uri.getPath());
-//            System.out.println("Query: " + uri.getQuery());
-//            System.out.println("Fragment: " + uri.getFragment());
-//
-//
-//            uri = new URI("http://www.filiptvrdon.sk");
-//            URL url = uri.toURL();
-//            System.out.println("URL: " + url);
+            URL url = new URL("http://www.filiptvrdon.sk");
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.connect();
 
-            URI baseUri = new URI("http://www.filiptvrdon.sk");
-            URI uri = new URI("/catalogue/phones?os=android#samsung");
-            URI resolvedUri = baseUri.resolve(uri);
-            URL url = resolvedUri.toURL();
-            System.out.println("URL: " + url);
+            BufferedReader inputStream = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
 
-        } catch (URISyntaxException e) {
-            System.out.println("URI Bad Syntax: " + e.getMessage());
+            String line = "";
+            while (line != null) {
+                line = inputStream.readLine();
+                System.out.println(line);
+            }
+            inputStream.close();
+
+
         } catch (MalformedURLException e) {
             System.out.println("URL Malformed: " + e.getMessage());
-        } catch (IllegalArgumentException e) {
+        } catch (IOException e) {
             System.out.println("URI Bad Syntax: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("URL Malformed: " + e.getMessage());
         }
 
 

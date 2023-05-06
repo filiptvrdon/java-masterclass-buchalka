4690cba444b163548c83bd8ae449cce83f4cd33b
Filip Tvrdoň <tvrdon.filip@gmail.com>
2023-05-06 07:38:08 +0200

    lesson 415 finished, some catching up

--- Lesson412HighLevelApis/src/sk/filiptvrdon/Main.java
+++ Lesson412HighLevelApis/src/sk/filiptvrdon/Main.java
@@ -4,24 +4,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
         try {
-            URL url = new URL("http://www.filiptvrdon.sk");
-            URLConnection urlConnection = url.openConnection();
-            urlConnection.setDoOutput(true);
-            urlConnection.connect();
+            URL url = new URL("https://www.flickr.com/services/feeds/photos_public.gne?tags=cats");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Chrome");
+            connection.setReadTimeout(5000);
 
-            BufferedReader inputStream = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response code: " + responseCode);
 
-            String line = "";
-            while (line != null) {
-                line = inputStream.readLine();
+            if (responseCode != 200) {
+                System.out.println("Error reading web page");
+                return;
+            }
+
+            BufferedReader inputReader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            String line;
+            while ((line = inputReader.readLine()) != null) {
                 System.out.println(line);
             }
-            inputStream.close();
+            inputReader.close();
+
+
+
 
 
         } catch (MalformedURLException e) {
